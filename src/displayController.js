65112da34a7b5e3f7db053c3bda46289a7d5ff3c
Serhii <stupanenko.serhii@gmail.com>
2023-10-14 00:18:55 +0300

    add displayImportant function

--- src/displayController.js
+++ src/displayController.js
@@ -2,24 +2,27 @@ import { getProjects, displayProjectTasks } from "./project";
 import { addProjectToPage, addTaskToPage } from "./domController";
 import { getTasks } from "./task";
 
+export let currentSection = "All Tasks";
+
+
 export const displayProjects = () => {
     const projects = getProjects();
     console.log("projects",projects)
     projects.forEach(project => {
-        // console.log(project);
         document.querySelector('.projectList').appendChild(addProjectToPage(project))
     })
-    // document.querySelector('.projectList').appendChild(projects)
 }
+// adjust this function to displayProjectTasks
 export const displayTasks = () => {
     const tasks = getTasks();
-    console.log("tasks",tasks)
+    console.log("Displaying tasks",tasks)
     tasks.forEach(task => {
+        // if(task.important) document.querySelector('')
         document.querySelector('.taskList').appendChild(addTaskToPage(task))
     })
 }
 
-export let currentSection = "All Tasks";
+
 export const displayCurrentSection = (className) => {
     const selectedProject = getProjects().filter(project => project.title == className.split('-')[1])[0] || [];
     currentSection = selectedProject.title || "All Tasks";
@@ -29,6 +32,15 @@ export const displayCurrentSection = (className) => {
     if(selectedProject.title) {
         displayProjectTasks(selectedProject);
     }else {
-        // displayTasks();
+        displayTasks();
     }
+}
+
+export const displayImportant = () => {
+    const tasks = getTasks();
+    tasks.forEach(task => {
+        if(task.important) {
+            document.querySelector('.taskList').appendChild(addTaskToPage(task))
+        }
+    })
 }
