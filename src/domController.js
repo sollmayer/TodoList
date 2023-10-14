7dda9ecef8e2af475470cf73e245abc7c609b3ad
Serhii <stupanenko.serhii@gmail.com>
2023-10-14 21:10:30 +0300

    implement task editing

--- src/domController.js
+++ src/domController.js
@@ -1,5 +1,5 @@
-import { createTask, addTask, deleteTask, toggleTaskImportant, toggleCompleteStatus, updateTaskStatus } from "./task";
-import { createProject, populateProjectList, deleteProject, updateProjectTaskList } from "./project";
+import { createTask, addTask, deleteTask, toggleTaskImportant, toggleCompleteStatus, updateTaskStatus, editTask } from "./task";
+import { createProject, populateProjectList, deleteProject, updateProjectTaskList, editProjectTaskList } from "./project";
 import { displayProjects, displayCurrentSection, currentSection, displayTasks, displayImportant } from "./displayController";
 
 const showTaskForm = document.querySelector('#showTaskForm');
@@ -7,8 +7,10 @@ const showProjectForm = document.querySelector('#showProjectForm');
 
 const taskDialog = document.querySelector("#task_dialog");
 const projectDialog = document.querySelector("#project_dialog");
+const editTaskDialog = document.querySelector("#editTask_dialog");
 
 const cancelTask = taskDialog.querySelector("#cancel_task");
+const cancelEditTask = taskDialog.querySelector("#cancel_task");
 const cancelProject = projectDialog.querySelector("#cancel_project");
 
 const taskForm = document.querySelector('#task_form');
@@ -16,6 +18,7 @@ const projectForm = document.querySelector('#project_form');
 
 const allTasksBtn = document.querySelector("#allTasks")
 const importantTasksBtn = document.querySelector("#importantTasks")
+
 
 export const addListeners = () => {
     showTaskForm.addEventListener('click', ()=>taskDialog.showModal())
@@ -25,6 +28,7 @@ export const addListeners = () => {
         e.preventDefault();
         taskDialog.close()
     })
+    cancelEditTask.addEventListener('click', (e)=>{e.preventDefault(); editTaskDialog.close()})
     cancelProject.addEventListener('click', (e)=> {
         e.preventDefault();
         projectDialog.close()
@@ -46,17 +50,9 @@ export const addListeners = () => {
         projectDialog.close();
     })
     
-    allTasksBtn.addEventListener('click', () => {
-        displayCurrentSection("All Tasks");
-        clearTaskList();
-        displayTasks();
-    })
+    allTasksBtn.addEventListener('click', () => displayCurrentSection("All Tasks"))
 
-    importantTasksBtn.addEventListener('click', ()=> {
-        displayCurrentSection("Important");
-        clearTaskList();
-        displayImportant();
-    })
+    importantTasksBtn.addEventListener('click', ()=> displayCurrentSection("Important"))
 }
 const saveProject = () => {
     const name = document.querySelector('#projectName').value
@@ -83,17 +79,8 @@ export const addProjectToPage = ({title}) => {
     deleteProjectBtn.setAttribute("data-title", title)
     deleteProjectBtn.addEventListener('click', ()=>deleteProject(deleteProjectBtn.dataset.title))
     
-   
 
-    // projectContainer.addEventListener('click', () => {
-    //     clearTaskList();
-    //     displayCurrentSection(projectContainer.classList.value);
-    // })
-
-    projectName.addEventListener('click', () => {
-        clearTaskList();
-        displayCurrentSection(projectContainer.classList.value)
-    })
+    projectName.addEventListener('click', () => displayCurrentSection(title))
     projectContainer.appendChild(projectName);
     projectContainer.appendChild(deleteProjectBtn);
 
@@ -108,6 +95,7 @@ export const addTaskToPage = ({title, description, dueDate,priority,important,co
     const taskDueDate = document.createElement('p');
     const taskPriority = document.createElement('p');
     const importantBtn = document.createElement('button');
+    const editBtn = document.createElement('button');
     const deleteTaskBtn = document.createElement('button');
     const completeCheckbox = document.createElement("input");
    
@@ -118,23 +106,36 @@ export const addTaskToPage = ({title, description, dueDate,priority,important,co
     taskDueDate.textContent = dueDate;
     taskPriority.textContent = priority;
 
+    
+    // importantBtn section
     importantBtn.textContent = "Important";
     if(important) importantBtn.classList.toggle('importantYellow');
+
+    let importantStatus = important;
+    importantBtn.addEventListener('click', () => {
+        importantBtn.classList.toggle('importantYellow');
+        updateTaskStatus(title, 'important', !importantStatus, updateProjectTaskList)
+        importantStatus = !importantStatus;
+    })
+
+    // editBtn section
+    editBtn.textContent = "Edit";
+    editBtn.addEventListener('click', ()=>{
+        showEditForm(title,description,dueDate,priority);
+    })
+    
+    // completeCheckbox section
     if(completed) {
         taskContainer.classList.toggle('completed');
         completeCheckbox.checked = true;
     }
-
-    importantBtn.addEventListener('click', () => {
-        importantBtn.classList.toggle('importantYellow');
-        // toggleTaskImportant(title);
-        updateTaskStatus(title, 'important', updateProjectTaskList)
-    })
+    let completeStatus = completed;
     completeCheckbox.addEventListener('change', ()=> {
         taskContainer.classList.toggle('completed');
-        // toggleCompleteStatus(title);
-        updateTaskStatus(title, 'completed', updateProjectTaskList)
+        updateTaskStatus(title, 'completed', !completeStatus, updateProjectTaskList)
+        completeStatus = !completeStatus;
     })
+
     deleteTaskBtn.textContent = "Delete Task";
     deleteTaskBtn.addEventListener('click', () => deleteTask(title))
 
@@ -144,6 +145,7 @@ export const addTaskToPage = ({title, description, dueDate,priority,important,co
     taskContainer.appendChild(taskDueDate);
     taskContainer.appendChild(taskPriority);
     taskContainer.appendChild(importantBtn);
+    taskContainer.appendChild(editBtn);
     taskContainer.appendChild(deleteTaskBtn);
     
     taskContainer.classList.add(`task-${title}`)
@@ -153,3 +155,31 @@ export const addTaskToPage = ({title, description, dueDate,priority,important,co
 export const appendTask = (container) => {
     document.querySelector('.taskList').appendChild(container);
 } 
+
+const showEditForm = (title,description,dueDate,priority) => {
+    let previousValues = {title,description,dueDate,priority}
+    editTaskDialog.showModal();
+    document.querySelector('#editTitle').value = title;
+    document.querySelector('#editDescription').value = description;
+    document.querySelector('#editDueDate').value = dueDate;
+    document.querySelector('#editPriority').value = priority;
+    editTaskDialog.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let newValues = {
+            'title': document.querySelector('#editTitle').value,
+            'description': document.querySelector('#editDescription').value,
+            'dueDate': document.querySelector('#editDueDate').value,
+            'priority': document.querySelector('#editPriority').value
+        }
+        console.log(newValues);
+        for(const [key,value] of Object.entries(newValues)){
+            if(value !== previousValues[key]){
+                updateTaskStatus(title, key, value, updateProjectTaskList);
+            }
+        }
+        editTaskDialog.close();
+        // clearTaskList();
+        console.log('showEditForm',currentSection);
+        displayCurrentSection(currentSection);
+    })
+}        
