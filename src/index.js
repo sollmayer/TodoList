032e014c6b1a526dfaa638293a7874daef1410ed
Serhii <stupanenko.serhii@gmail.com>
2023-10-17 16:59:56 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import { addListeners } from "./domController";
 import { displayCurrentSection, displayProjects, displayTasks } from "./displayController";
 addListeners();
 displayProjects();
-displayCurrentSection('All Tasks');
+// displayCurrentSection('All Tasks');
+displayCurrentSection("All Tasks",document.querySelector('#allTasks'))
